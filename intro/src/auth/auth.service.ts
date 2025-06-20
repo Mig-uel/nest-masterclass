5aac426a3d1b02d4c6b1f0798b4b1b4fa8cf0e62
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-20 17:59:13 -0400

    feat: implement AuthService with login and authentication methods
    - Added constructor to inject UsersService using forwardRef
    - Implemented login method to authenticate users and return a token
    - Added isAuth method to check user authentication status

--- intro/src/auth/auth.service.ts
+++ intro/src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+
+  /**
+   * Login
+   */
+  login(email: string, password: string, uid: string) {
+    // Check if user exists in database
+    const user = this.usersService.findOneById(uid);
+
+    // Login logic
+    // Token
+    return {
+      token: crypto.randomUUID(),
+      user,
+    };
+  }
+
+  /**
+   * Check if user is authenticated
+   */
+  isAuth() {
+    return true;
+  }
+}
