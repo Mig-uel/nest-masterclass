cc8a28a878edd8fe59c77eb94356d3e1b4495bd6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-20 19:31:30 -0400

    feat: integrate Swagger for API documentation
    Added Swagger setup to the project
    Configured Swagger module for API docs generation
    Enabled interactive API documentation endpoint

--- intro/src/main.ts
+++ intro/src/main.ts
@@ -1,11 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Globally set up ValidationPipe
+  /**
+   * Globally setup ValidationPipe
+   */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // strips unknown properties from request body
@@ -13,6 +17,17 @@ async function bootstrap() {
       transform: true, // transforms an incoming request to an instance of the dto class after validation
     }),
   );
+
+  /**
+   * Swagger config
+   */
+  const config = new DocumentBuilder().setVersion('1.0').build();
+
+  // Instantiate Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Use document to complete setup
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
