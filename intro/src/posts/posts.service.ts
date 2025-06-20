8966bbf6bb36301eb616dc226ef0e3579d2b54e1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-20 16:47:44 -0400

    feat: implement findAll method in PostsService
    - Retrieve user details using UsersService
    - Return posts associated with the user
    - Handle case where user is not found

--- intro/src/posts/posts.service.ts
+++ intro/src/posts/posts.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from './../users/users.service';
 
 @Injectable()
 export class PostsService {
-  findAll(userId: string) {
-    return { userId };
+  constructor(private readonly usersService: UsersService) {}
+
+  findAll(uid: string) {
+    const user = this.usersService.findOneById(uid);
+
+    if (!user) return { message: 'No user found!' };
+
+    return [
+      {
+        user,
+        title: 'First Post',
+        content: 'This is the content of the first post.',
+      },
+      {
+        user,
+        title: 'Second Post',
+        content: 'This is the content of the second post.',
+      },
+    ];
   }
 }
