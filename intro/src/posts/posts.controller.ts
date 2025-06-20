e24f5f41e9063ac7d6e2a897b83b07ca415aa829
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-20 16:37:10 -0400

    feat: add getPosts method to PostsController
    - Implemented a new endpoint to retrieve posts by user ID.
    - Enhances the PostsController functionality for better data access.

--- intro/src/posts/posts.controller.ts
+++ intro/src/posts/posts.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get('{/:uid}')
+  getPosts(@Param('uid') uid: string) {
+    return this.postsService.findAll(uid);
+  }
 }
