bf99116003aaa1c09dbec43116e49093aae41235
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-20 10:30:26 -0400

    feat: add detailed documentation for getUsers endpoint
    - Enhance the JSDoc comments for the getUsers method
    - Clarify parameters and query options for better understanding

--- intro/src/users/users.controller.ts
+++ intro/src/users/users.controller.ts
@@ -11,6 +11,17 @@ import {
 
 @Controller('users')
 export class UsersController {
+  /**
+   * Endpoint - /users/id?limit=10&page=1
+   * Param id - optional, convert to integer, cannot have a default value
+   * Query limit - integer, default value of 10
+   * Query page - integer, default value of 1
+   *
+   * ==> Use Cases
+   * /users/ => returns all users with default pagination
+   * /users/1234 => returns one user with id of 1234
+   * /users?limit=10&page=2 => returns page 2 with limit of 10
+   */
   @Get('{/:id}')
   getUsers(
     @Param('id') id?: string,
