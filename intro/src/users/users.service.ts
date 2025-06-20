affadc18c220f14f26b10f1222c040d90ba00f1b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-20 17:59:09 -0400

    feat: integrate AuthService into UsersService
    - Inject AuthService using forwardRef to resolve circular dependency
    - Enhance findAll method to check authentication status

--- intro/src/users/users.service.ts
+++ intro/src/users/users.service.ts
@@ -1,12 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
+
   /**
    * Find all users
    */
   findAll(limit: number, page: number) {
     console.log(limit, page);
+
+    const isAuth = this.authService.isAuth();
+    console.log(isAuth);
+
     return [
       { firstName: 'Alice', email: 'alice@example.com' },
       { firstName: 'Bob', email: 'bob@example.com' },
