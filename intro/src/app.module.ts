8d0f2ce7198ff33f946d6ed046c4c9dd55d0c360
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-21 14:47:38 -0400

    feat: configure TypeORM with PostgreSQL in AppModule
    - Added TypeOrmModule configuration for PostgreSQL database
    - Integrated ConfigModule for environment variable management

--- intro/src/app.module.ts
+++ intro/src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      synchronize: true,
+      entities: [],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
