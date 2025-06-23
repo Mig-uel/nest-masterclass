40a8450df5a60447251956c6fcea7c09020038b4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-23 18:21:29 -0400

    feat: inject MetaOptionsService and PostsRepository in PostsService
    - Enhance PostsService to include MetaOptionsService for handling
      meta options during post creation.
    - Inject PostsRepository to facilitate post management.

--- intro/src/posts/posts.service.ts
+++ intro/src/posts/posts.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MetaOptionsService } from 'src/meta-options/meta-options.service';
+import type { Repository } from 'typeorm';
 import { UsersService } from './../users/users.service';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { Post as P } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
-  constructor(private readonly usersService: UsersService) {}
+  /**
+   * Inject Users Service, Meta Options Service and Posts Repository
+   * @param usersService
+   * @param metaOptionsService
+   * @param postsRepository
+   */
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly metaOptionsService: MetaOptionsService,
+    @InjectRepository(P)
+    private readonly postsRepository: Repository<P>,
+  ) {}
 
   findAll() {
     return [{}];
@@ -26,5 +42,37 @@ export class PostsService {
         content: 'This is the content of the second post.',
       },
     ];
+  }
+
+  /**
+   * Method for creating a post
+   * @param createPostDto
+   */
+  @Post()
+  async create(createPostDto: CreatePostDto) {
+    // Create metaOptions
+    const metaOptions = createPostDto.metaOptions
+      ? await this.metaOptionsService.create(createPostDto.metaOptions)
+      : null;
+
+    // Create post
+    const post = this.postsRepository.create({
+      content: createPostDto.content,
+      featuredImageUrl: createPostDto.featuredImageUrl,
+      postType: createPostDto.postType,
+      publishOn: createPostDto.publishOn,
+      schema: createPostDto.schema,
+      slug: createPostDto.slug,
+      status: createPostDto.status,
+      title: createPostDto.title,
+    });
+
+    // Add metaOptions
+    if (metaOptions) {
+      post.metaOption = metaOptions;
+    }
+
+    // Return the post
+    return this.postsRepository.save(post);
   }
 }
