f8908e8f03b5421e4fd2444d183fbbcbb6a794cd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-23 17:37:48 -0400

    feat: implement create route in MetaOptionsController
    - Add create method to handle post meta option creation
    - Utilize MetaOptionsService for processing the request

--- intro/src/meta-options/meta-options.controller.ts
+++ intro/src/meta-options/meta-options.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateMetaOptionsDto } from './dtos/create-meta-options.dto';
+import { MetaOptionsService } from './meta-options.service';
 
 @Controller('meta-options')
-export class MetaOptionsController {}
+export class MetaOptionsController {
+  constructor(private readonly metaOptionsService: MetaOptionsService) {}
+
+  /**
+   * Route for creating a post meta option
+   * @param createPostMetaOptionsDto
+   * @returns
+   */
+  @Post()
+  create(@Body() createPostMetaOptionsDto: CreateMetaOptionsDto) {
+    return this.metaOptionsService.create(createPostMetaOptionsDto);
+  }
+}
