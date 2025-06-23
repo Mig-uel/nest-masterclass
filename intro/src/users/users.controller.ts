bec50908883864560b05d3e6c68a14dd8545a1b7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-23 12:29:01 -0400

    refactor: clean up UsersController and rename createUsers method
    - Removed outdated comments and documentation for clarity
    - Renamed createUsers method to createUser for consistency

--- intro/src/users/users.controller.ts
+++ intro/src/users/users.controller.ts
@@ -22,17 +22,6 @@ export class UsersController {
   // Inject Users Service
   constructor(private readonly usersService: UsersService) {}
 
-  /**
-   * Endpoint - /users/id?limit=10&page=1
-   * Param id - optional, convert to integer, cannot have a default value
-   * Query limit - integer, default value of 10
-   * Query page - integer, default value of 1
-   *
-   * ==> Use Cases
-   * /users/ => returns all users with default pagination
-   * /users/1234 => returns one user with id of 1234
-   * /users?limit=10&page=2 => returns page 2 with limit of 10
-   */
   @Get('{/:id}')
   @ApiOperation({
     summary: 'Fetches a list of registered users on the application',
@@ -68,7 +57,7 @@ export class UsersController {
   }
 
   @Post()
-  createUsers(
+  createUser(
     @Body() createUserDto: CreateUserDto,
     @Headers() _: any, // Get headers from request
     @Ip() ip: string, // Get IP address of the request
