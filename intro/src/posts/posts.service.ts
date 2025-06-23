4cdae401c632684678e419a1a127b7c6406d53c9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-23 19:32:49 -0400

    refactor: remove metaOptions handling from create method
    - Simplified the create method by removing metaOptions logic.
    - This change improves code clarity and focuses on post creation.

--- intro/src/posts/posts.service.ts
+++ intro/src/posts/posts.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MetaOptionsService } from 'src/meta-options/meta-options.service';
 import type { Repository } from 'typeorm';
 import { UsersService } from './../users/users.service';
 import { CreatePostDto } from './dtos/create-post.dto';
@@ -16,7 +15,6 @@ export class PostsService {
    */
   constructor(
     private readonly usersService: UsersService,
-    private readonly metaOptionsService: MetaOptionsService,
     @InjectRepository(P)
     private readonly postsRepository: Repository<P>,
   ) {}
@@ -50,27 +48,8 @@ export class PostsService {
    */
   @Post()
   async create(createPostDto: CreatePostDto) {
-    // Create metaOptions
-    const metaOptions = createPostDto.metaOptions
-      ? await this.metaOptionsService.create(createPostDto.metaOptions)
-      : null;
-
     // Create post
-    const post = this.postsRepository.create({
-      content: createPostDto.content,
-      featuredImageUrl: createPostDto.featuredImageUrl,
-      postType: createPostDto.postType,
-      publishOn: createPostDto.publishOn,
-      schema: createPostDto.schema,
-      slug: createPostDto.slug,
-      status: createPostDto.status,
-      title: createPostDto.title,
-    });
-
-    // Add metaOptions
-    if (metaOptions) {
-      post.metaOption = metaOptions;
-    }
+    const post = this.postsRepository.create(createPostDto);
 
     // Return the post
     return this.postsRepository.save(post);
