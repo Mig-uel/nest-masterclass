d10b1128d5b9923d0fef60c291c4c78b7e407f9a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-01 21:54:41 -0400

    feat: add TagsModule to PostsModule imports
    - Integrates TagsModule to enable tag functionality within posts.

--- intro/src/posts/posts.module.ts
+++ intro/src/posts/posts.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MetaOptionsModule } from 'src/meta-options/meta-options.module';
 import { UsersModule } from 'src/users/users.module';
+import { TagsModule } from '../tags/tags.module';
 import { Post } from './entities/post.entity';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 
 @Module({
   controllers: [PostsController],
-  imports: [MetaOptionsModule, UsersModule, TypeOrmModule.forFeature([Post])],
+  imports: [
+    MetaOptionsModule,
+    UsersModule,
+    TypeOrmModule.forFeature([Post]),
+    TagsModule,
+  ],
   providers: [PostsService],
 })
 export class PostsModule {}
