37708ca9f53003dd177d485775f3e7a7580d822f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-04 17:50:39 -0400

    feat: add database configuration file
    - Introduces a new configuration file for database settings
    - Allows dynamic configuration using environment variables

---
+++ intro/src/config/database.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  host: process.env.PG_HOST || 'localhost',
+  port: parseInt(process.env.PG_PORT as string) || 5432,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  synchronize: process.env.DB_SYNC === 'true',
+  autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === 'true',
+}));
