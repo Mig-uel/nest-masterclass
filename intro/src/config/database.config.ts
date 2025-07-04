cfa7867badfbd6ca079e5903d7d4ce1b3aecbb71
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-04 18:27:20 -0400

    fix: correct environment variable reference for DB port
    - Updated the database configuration to use the correct
      environment variable for the database port.

--- intro/src/config/database.config.ts
+++ intro/src/config/database.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   host: process.env.PG_HOST || 'localhost',
-  port: parseInt(process.env.PG_PORT as string) || 5432,
+  port: parseInt(process.env.DB_PORT as string) || 5432,
   username: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
