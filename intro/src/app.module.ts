d1c2a35c1517d89c99f17ad8a29e52a030b43d70
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-04 20:05:32 -0400

    refactor: remove unused AppController and AppService
    - Cleaned up the application by deleting the AppController and
      AppService as they were not utilized in the current module setup.

--- intro/src/app.module.ts
+++ intro/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import AppConfig from './config/app.config';
 import DatabaseConfig from './config/database.config';
@@ -45,7 +43,5 @@ const ENV = process.env.NODE_ENV;
     TagsModule,
     MetaOptionsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- intro/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-// Data Layer Logic
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
