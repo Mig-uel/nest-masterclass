c49f4bd7a20f497d6c3e183daf66e00a8f35465c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-12 18:57:19 -0400

    feat: add UsersCreateMany provider to UsersModule
    - Integrates UsersCreateMany provider for bulk user creation
    - Enhances user management capabilities within the module

--- intro/src/users/users.module.ts
+++ intro/src/users/users.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ProfileConfig from './config/profile.config';
 import { User } from './entities/user.entity';
+import { UsersCreateMany } from './providers/users-create-many';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersCreateMany],
   exports: [UsersService],
   imports: [
     TypeOrmModule.forFeature([User]),
