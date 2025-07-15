7d0f54afbcc1bd928c406ae783ffe52e55da6511
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-14 22:13:40 -0400

    feat: enhance pagination logic with URL generation
    - Inject request object to create base URLs for pagination links
    - Calculate total items and pages for improved pagination response
    - Return structured response including data, meta, and links

--- intro/src/common/pagination/providers/pagination.provider.ts
+++ intro/src/common/pagination/providers/pagination.provider.ts
@@ -1,18 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 import type { Repository } from 'typeorm';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
+import type { Paginated } from '../interfaces/paginated.interface';
 
 @Injectable()
 export class PaginationProvider {
+  constructor(@Inject(REQUEST) private readonly request: Request) {}
+
   async paginateQuery<T extends Record<string, any>>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
-  ) {
+  ): Promise<Paginated> {
     const { limit, page } = paginationQuery;
 
-    return await repository.find({
+    const results = await repository.find({
       skip: (page! - 1) * limit!,
       take: limit,
     });
+
+    // TODO => implement cleaner url building logic
+    // Create the request URLs
+    const baseUrl =
+      this.request.protocol + '://' + this.request.headers.host + '/';
+
+    const linksBase = new URL(this.request.url, baseUrl);
+
+    // Calculate page numbers
+    const totalItems = await repository.count();
+    const totalPages = Math.ceil(totalItems / limit!);
+    const nextPage = page === totalPages ? page : page! + 1;
+    const previousPage = page === 1 ? page : page! - 1;
+
+    // Response
+    const response: Paginated<typeof results> = {
+      data: results,
+      meta: {
+        currentPage: page!,
+        itemsPerPage: limit!,
+        totalItems,
+        totalPages,
+      },
+      links: {
+        current: `${linksBase.origin}${linksBase.pathname}?limit=${limit}&page=${page}`,
+        first: `${linksBase.origin}${linksBase.pathname}?limit=${limit}&page=1`,
+        last: `${linksBase.origin}${linksBase.pathname}?limit=${limit}&page=${totalPages}`,
+        next: `${linksBase.origin}${linksBase.pathname}?limit=${limit}&page=${nextPage}`,
+        previous: `${linksBase.origin}${linksBase.pathname}?limit=${limit}&page=${previousPage}`,
+      },
+    };
+
+    return response;
   }
 }
