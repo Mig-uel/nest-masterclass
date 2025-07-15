d4cb37a84a5e900c050f82230c11c24e7beda0a0
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-14 21:36:50 -0400

    feat: implement pagination logic in PaginationProvider
    - Add paginateQuery method to handle pagination logic
    - Utilize PaginationQueryDto for query parameters

--- intro/src/common/pagination/providers/pagination.provider.ts
+++ intro/src/common/pagination/providers/pagination.provider.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import type { Repository } from 'typeorm';
+import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 
 @Injectable()
-export class PaginationProvider {}
+export class PaginationProvider {
+  async paginateQuery<T extends Record<string, any>>(
+    paginationQuery: PaginationQueryDto,
+    repository: Repository<T>,
+  ) {
+    const { limit, page } = paginationQuery;
+
+    return await repository.find({
+      skip: (page! - 1) * limit!,
+      take: limit,
+    });
+  }
+}
