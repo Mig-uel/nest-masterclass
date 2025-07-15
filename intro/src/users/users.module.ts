60426a1771e766dcbbaf5d1ddb328ac8efe37d2d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-15 04:00:02 -0400

    feat: add CreateUserProvider to UsersModule
    - Include CreateUserProvider in the providers array
    - Update imports to include AuthModule with forwardRef

--- intro/src/users/users.module.ts
+++ intro/src/users/users.module.ts
@@ -1,21 +1,24 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { PaginationModule } from 'src/common/pagination/pagination.module';
 import ProfileConfig from './config/profile.config';
 import { User } from './entities/user.entity';
+import { CreateUserProvider } from './providers/create-user.provider';
 import { UsersCreateMany } from './providers/users-create-many';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersCreateMany],
+  providers: [UsersService, UsersCreateMany, CreateUserProvider],
   exports: [UsersService],
   imports: [
     TypeOrmModule.forFeature([User]),
     ConfigModule.forFeature(ProfileConfig),
     PaginationModule,
+    forwardRef(() => AuthModule),
   ],
 })
 export class UsersModule {}
