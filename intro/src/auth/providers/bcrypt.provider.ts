ed4d447850447938bc1eb5baea9221004b592006
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-15 03:42:50 -0400

    feat: implement password hashing and comparison methods
    - Added hashPassword method to generate hashed passwords
    - Implemented comparePassword method for password validation

--- intro/src/auth/providers/bcrypt.provider.ts
+++ intro/src/auth/providers/bcrypt.provider.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import type { HashingProvider } from './hashing.provider';
 
 @Injectable()
 export class BcryptProvider implements HashingProvider {
-  hashPassword(data: string | Buffer): Promise<string> {
-    throw new Error('Method not implemented');
+  async hashPassword(data: string | Buffer): Promise<string> {
+    // Generate a salt
+    const salt = await bcrypt.genSalt();
+
+    // Return hashed password
+    return bcrypt.hash(data, salt);
   }
 
-  comparePassword(data: string | Buffer, encrypted: string): Promise<boolean> {
-    throw new Error('Method not implemented');
+  async comparePassword(
+    data: string | Buffer,
+    encrypted: string,
+  ): Promise<boolean> {
+    // Return compare boolean
+    return await bcrypt.compare(data, encrypted);
   }
 }
