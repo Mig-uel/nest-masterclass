30dff413e29165100f2e8dd42b5bd31b85394f67
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-15 03:47:24 -0400

    feat: update AuthModule to use HashingProvider
    - Replace direct BcryptProvider usage with HashingProvider
    - Ensure consistent provider interface for hashing services

--- intro/src/auth/auth.module.ts
+++ intro/src/auth/auth.module.ts
@@ -3,11 +3,18 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { BcryptProvider } from './providers/bcrypt.provider';
+import { HashingProvider } from './providers/hashing.provider';
 
 @Module({
-  providers: [AuthService, BcryptProvider],
+  providers: [
+    AuthService,
+    {
+      provide: HashingProvider,
+      useClass: BcryptProvider,
+    },
+  ],
   controllers: [AuthController],
   imports: [forwardRef(() => UsersModule)],
-  exports: [AuthService],
+  exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
