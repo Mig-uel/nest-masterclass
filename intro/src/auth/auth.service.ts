7e182837729039df949c6cc4a9d923da94e930fc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-15 04:37:15 -0400

    refactor: simplify AuthService constructor by removing UsersService
    - Removed UsersService injection from AuthService constructor
    - Streamlined dependencies for better maintainability

--- intro/src/auth/auth.service.ts
+++ intro/src/auth/auth.service.ts
@@ -1,15 +1,10 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { Injectable } from '@nestjs/common';
 import { SignInDto } from './dtos/sign-in.dto';
 import { SignInProvider } from './providers/sign-in.provider';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @Inject(forwardRef(() => UsersService))
-    private readonly usersService: UsersService,
-    private readonly signInProvider: SignInProvider,
-  ) {}
+  constructor(private readonly signInProvider: SignInProvider) {}
 
   async signIn(signInDto: SignInDto) {
     return await this.signInProvider.signIn(signInDto);
