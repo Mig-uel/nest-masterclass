428d8642e2f83409ad4d3faaf4e79f9c532d08af
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-15 22:20:56 -0400

    feat: integrate JWT configuration into AuthModule
    - Add ConfigModule and JwtModule to AuthModule imports
    - Enable JWT configuration for authentication features

--- intro/src/auth/auth.module.ts
+++ intro/src/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import JWTConfig from './config/jwt.config';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { HashingProvider } from './providers/hashing.provider';
 import { SignInProvider } from './providers/sign-in.provider';
+import jwtConfig from './config/jwt.config';
 
 @Module({
   providers: [
@@ -16,7 +20,11 @@ import { SignInProvider } from './providers/sign-in.provider';
     SignInProvider,
   ],
   controllers: [AuthController],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(JWTConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
