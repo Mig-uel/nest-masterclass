8dafd7c4672dd1e1975e5c31f1c0a5654c7a2fb4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-16 01:04:06 -0400

    fix: remove redundant ConfigModule import in AuthModule
    - Cleaned up imports by removing unused ConfigModule.
    - Ensured JWTConfig is imported correctly from the config directory.

--- intro/src/auth/auth.module.ts
+++ intro/src/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import JWTConfig from 'src/config/jwt.config';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import JWTConfig from './config/jwt.config';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { HashingProvider } from './providers/hashing.provider';
 import { SignInProvider } from './providers/sign-in.provider';
-import jwtConfig from './config/jwt.config';
 
 @Module({
   providers: [
@@ -22,8 +20,7 @@ import jwtConfig from './config/jwt.config';
   controllers: [AuthController],
   imports: [
     forwardRef(() => UsersModule),
-    ConfigModule.forFeature(JWTConfig),
-    JwtModule.registerAsync(jwtConfig.asProvider()),
+    JwtModule.registerAsync(JWTConfig.asProvider()),
   ],
   exports: [AuthService, HashingProvider],
 })
