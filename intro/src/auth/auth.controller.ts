baa88b74c7ca4cb8068ca56b45e455e202bf0667
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-19 12:13:55 -0400

    feat: add Auth decorator to signIn endpoint for security
    - Implemented Auth decorator with AuthType.None
    - Enhances security handling for the sign-in process

--- intro/src/auth/auth.controller.ts
+++ intro/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Auth } from './decorators/auth.decorator';
 import { SignInDto } from './dtos/sign-in.dto';
+import { AuthType } from './enums/auth-type.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -8,6 +10,7 @@ export class AuthController {
 
   @Post('sign-in')
   @HttpCode(HttpStatus.OK)
+  @Auth(AuthType.None)
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
