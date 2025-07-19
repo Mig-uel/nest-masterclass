166cda59124bf74d36e11667e22f8dc39170bf10
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-19 11:57:07 -0400

    refactor: reorganize imports and enhance canActivate logic
    - Grouped imports for better readability
    - Improved canActivate method to handle multiple auth types
    - Ensured UnauthorizedException is thrown when no guards allow access

--- intro/src/auth/guards/authentication/authentication.guard.ts
+++ intro/src/auth/guards/authentication/authentication.guard.ts
@@ -1,8 +1,13 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
-import { AccessTokenGuard } from '../access-token/access-token.guard';
+import { AUTH_TYPE_KEY } from 'src/auth/constants/auth.constants';
 import { AuthType } from 'src/auth/enums/auth-type.enum';
+import { AccessTokenGuard } from '../access-token/access-token.guard';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
@@ -24,10 +29,23 @@ export class AuthenticationGuard implements CanActivate {
     };
   }
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log(this.authTypeGuardMap);
-    return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Get all authTypes set by Auth our decorator from Reflector
+    const authTypes = this.reflector.getAllAndOverride<AuthType[]>(
+      AUTH_TYPE_KEY,
+      [context.getHandler(), context.getClass()],
+    ) ?? [AuthenticationGuard.defaultAuthType];
+
+    // Create array of all guards
+    const guards = authTypes.map((type) => this.authTypeGuardMap[type]).flat();
+
+    // Loop through each of the guards and fire the canActivate methods
+    for (const instance of guards) {
+      const canActivate = await Promise.resolve(instance.canActivate(context));
+
+      if (canActivate) return true;
+    }
+
+    throw new UnauthorizedException();
   }
 }
