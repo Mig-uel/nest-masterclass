e0b71fe9010989135b325f0082b2be33d194e274
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-20 21:02:11 -0400

    feat: implement refreshTokens endpoint in AuthController
    - Adds a new POST endpoint for refreshing tokens
    - Utilizes RefreshTokenDto for request validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Auth } from './decorators/auth.decorator';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
 import { SignInDto } from './dtos/sign-in.dto';
 import { AuthType } from './enums/auth-type.enum';
 
@@ -13,5 +14,12 @@ export class AuthController {
   @Auth(AuthType.None)
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  @Post('refresh-tokens')
+  @HttpCode(HttpStatus.CREATED)
+  @Auth(AuthType.None)
+  refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto);
   }
 }
