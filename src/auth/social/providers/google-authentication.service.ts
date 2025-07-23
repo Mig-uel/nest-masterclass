d2d087eb1668be72b168769a70dccea22f14eb82
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-23 13:35:14 -0400

    feat: enhance GoogleAuthenticationService with user retrieval
    - Inject UsersService and GenerateTokensProvider for user handling
    - Implement authenticate method to verify Google token and manage user
    - Generate JWT for existing users or handle new user creation

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,7 +1,16 @@
-import { Inject, Injectable, type OnModuleInit } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+  type OnModuleInit,
+} from '@nestjs/common';
 import type { ConfigType } from '@nestjs/config';
 import { OAuth2Client } from 'google-auth-library';
+import { GenerateTokensProvider } from 'src/auth/providers/generate-tokens.provider';
 import JWTConfig from 'src/config/jwt.config';
+import { UsersService } from 'src/users/users.service';
+import { GoogleTokenDto } from '../dtos/google-token.dto';
 
 @Injectable()
 export class GoogleAuthenticationService implements OnModuleInit {
@@ -10,6 +19,9 @@ export class GoogleAuthenticationService implements OnModuleInit {
   constructor(
     @Inject(JWTConfig.KEY)
     private readonly jwtConfig: ConfigType<typeof JWTConfig>,
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+    private readonly generateTokensProvider: GenerateTokensProvider,
   ) {}
 
   /**
@@ -21,5 +33,26 @@ export class GoogleAuthenticationService implements OnModuleInit {
     const clientSecret = this.jwtConfig.googleClientSecret;
 
     this.oAuthClient = new OAuth2Client(clientId, clientSecret);
+  }
+
+  async authenticate(googleTokenDto: GoogleTokenDto) {
+    // Verify Google token sent by the user
+    const loginTicket = await this.oAuthClient.verifyIdToken({
+      idToken: googleTokenDto.token,
+    });
+
+    // Extract the payload from Google JWT
+    const payload = loginTicket.getPayload();
+
+    if (!payload) throw new UnauthorizedException();
+
+    // Find the user in the database using the GoogleId
+    const user = await this.usersService.findOneByGoogleId(payload.sub);
+
+    // If googleId exists in database, generate JWT
+    if (user) return this.generateTokensProvider.generateTokens(user);
+
+    // If not, create a new user and then generate JWT
+    // If all else fails, throw Unauthorized exception
   }
 }
