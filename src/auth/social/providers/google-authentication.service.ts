9f70497a9542010dae5ee6c2d7aa50d7beba05da
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-23 13:11:51 -0400

    feat: implement GoogleAuthenticationService with OAuth2Client
    - Add GoogleAuthenticationService class to handle Google OAuth2
    - Implement OnModuleInit lifecycle hook for client initialization
    - Inject JWT configuration for client ID and secret management

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, type OnModuleInit } from '@nestjs/common';
+import type { ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import JWTConfig from 'src/config/jwt.config';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+  private oAuthClient: OAuth2Client;
+
+  constructor(
+    @Inject(JWTConfig.KEY)
+    private readonly jwtConfig: ConfigType<typeof JWTConfig>,
+  ) {}
+
+  /**
+   * NestJS provides us with a lifecycle hook: OnModuleInit
+   * That guarantees execution after dependency injection
+   */
+  onModuleInit() {
+    const clientId = this.jwtConfig.googleClientId;
+    const clientSecret = this.jwtConfig.googleClientSecret;
+
+    this.oAuthClient = new OAuth2Client(clientId, clientSecret);
+  }
+}
