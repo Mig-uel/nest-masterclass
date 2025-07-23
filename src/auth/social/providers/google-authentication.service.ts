776434579c55592abcbe27125e6f392643f0cbef
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-23 16:16:54 -0400

    refactor: streamline Google token authentication process
    - Removed console logs for cleaner code
    - Improved error handling for token verification
    - Enhanced readability by restructuring code flow

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -34,34 +34,45 @@ export class GoogleAuthenticationService implements OnModuleInit {
   onModuleInit() {}
 
   async authenticate(googleTokenDto: GoogleTokenDto) {
-    console.log(googleTokenDto.token);
-    // Verify Google token sent by the user
-    const loginTicket = await this.oAuthClient.verifyIdToken({
-      idToken: googleTokenDto.token,
-    });
+    try {
+      // Verify Google token sent by the user
+      const loginTicket = await this.oAuthClient.verifyIdToken({
+        idToken: googleTokenDto.token,
+      });
 
-    console.log(loginTicket);
+      // Get payload from loginTicket
+      const payload = loginTicket.getPayload();
 
-    // Get payload from loginTicket
-    const payload = loginTicket.getPayload();
+      if (!payload) throw new UnauthorizedException();
 
-    if (!payload) throw new UnauthorizedException();
+      // Extract the payload from Google JWT
+      const {
+        email,
+        sub: googleId,
+        given_name: firstName,
+        family_name: lastName,
+      } = payload;
 
-    // Extract the payload from Google JWT
-    const {
-      email,
-      sub: googleId,
-      given_name: firstName,
-      family_name: lastName,
-    } = payload;
+      // Find the user in the database using the GoogleId
+      const user = await this.usersService.findOneByGoogleId(googleId);
 
-    // Find the user in the database using the GoogleId
-    const user = await this.usersService.findOneByGoogleId(googleId);
+      // If googleId exists in database, generate JWT
+      if (user) return this.generateTokensProvider.generateTokens(user);
 
-    // If googleId exists in database, generate JWT
-    if (user) return this.generateTokensProvider.generateTokens(user);
+      if (!email || !googleId || !firstName || !lastName)
+        throw new UnauthorizedException();
 
-    // If not, create a new user and then generate JWT
-    // If all else fails, throw Unauthorized exception
+      // If not, create a new user and then generate JWT
+      const newUser = await this.usersService.createGoogleUser({
+        email,
+        googleId,
+        firstName,
+        lastName,
+      });
+
+      return this.generateTokensProvider.generateTokens(newUser);
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
   }
 }
