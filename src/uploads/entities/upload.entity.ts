4ba88f13479a50e4863dbaa586c518566a0304d9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-25 15:50:00 -0400

    feat: create Upload entity for file management
    - Define Upload entity with properties for file metadata
    - Integrate FileTypes enum for type handling
    - Set default file type to IMAGE

---
+++ src/uploads/entities/upload.entity.ts
@@ -0,0 +1,47 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { FileTypes } from '../enums/file-types.enum';
+
+@Entity()
+export class Upload {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 1024,
+  })
+  name: string;
+
+  @Column({
+    length: 1024,
+  })
+  path: string;
+
+  @Column({
+    type: 'enum',
+    enum: FileTypes,
+    default: FileTypes.IMAGE,
+  })
+  type: FileTypes;
+
+  @Column({
+    length: 128,
+  })
+  mime: string;
+
+  @Column({
+    length: 1024,
+  })
+  size: number;
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
