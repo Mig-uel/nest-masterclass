0756ed5349f944cecc9430ad29a95cf3db7ed0d9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-25 16:48:21 -0400

    feat: implement uploadFile method in UploadsController
    - Add file upload functionality to the uploads controller
    - Include necessary decorators for file handling and API documentation

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiHeaders, ApiOperation } from '@nestjs/swagger';
+import { UploadsService } from './providers/uploads.service';
 
 @Controller('uploads')
-export class UploadsController {}
+export class UploadsController {
+  /**
+   * Inject UploadsService
+   */
+  constructor(private readonly uploadsService: UploadsService) {}
+
+  @Post('file')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiHeaders([
+    {
+      name: 'Content-Type',
+      description: 'multipart/form-data',
+    },
+    {
+      name: 'Authorization',
+      description: 'Bearer Token',
+    },
+  ])
+  @ApiOperation({
+    summary: 'Upload a new image to the server',
+  })
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return this.uploadsService.uploadFile(file);
+  }
+}
