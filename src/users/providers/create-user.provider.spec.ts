5b742d6d9a224a1ca7d72b9bfc6e1288b712d199
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 09:29:04 -0400

    refactor: improve CreateUserProvider tests with mock repository
    - Introduced a mock repository for User entity to enhance test isolation
    - Updated the test setup to utilize the mock repository for better clarity

--- src/users/providers/create-user.provider.spec.ts
+++ src/users/providers/create-user.provider.spec.ts
@@ -2,12 +2,24 @@ import { Test, type TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { HashingProvider } from 'src/auth/providers/hashing.provider';
 import { MailService } from 'src/mail/providers/mail.service';
-import { DataSource } from 'typeorm';
+import { DataSource, type ObjectLiteral, type Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { CreateUserProvider } from './create-user.provider';
 
+type MockRepository<T extends ObjectLiteral> = Partial<
+  Record<keyof Repository<T>, jest.Mock>
+>;
+const createMockRepository = <
+  T extends ObjectLiteral,
+>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
+
 describe('CreateUserProvider', () => {
   let provider: CreateUserProvider;
+  let usersRepository: MockRepository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,12 +39,13 @@ describe('CreateUserProvider', () => {
         },
         {
           provide: getRepositoryToken(User),
-          useValue: {},
+          useValue: createMockRepository(),
         },
       ],
     }).compile();
 
     provider = module.get<CreateUserProvider>(CreateUserProvider);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
