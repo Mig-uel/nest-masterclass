95828e0fb753d730b07ffaa7a35a816a2d087b31
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 05:14:37 -0400

    feat: implement MailerModule with EJS template support
    - Added MailerModule configuration for email transport.
    - Integrated EJS adapter for email templating.
    - Set up mail defaults and template directory.

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,42 @@
-import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { Global, Module } from '@nestjs/common';
+import type { ConfigType } from '@nestjs/config';
+import { join } from 'path';
+import MailConfig from 'src/config/mail.config';
 import { MailService } from './providers/mail.service';
 
+@Global()
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [MailConfig.KEY],
+      useFactory(mailConfig: ConfigType<typeof MailConfig>) {
+        return {
+          transport: {
+            host: mailConfig.mailHost!,
+            secure: false,
+            port: 2525,
+            auth: {
+              user: mailConfig.smtpUsername,
+              pass: mailConfig.smtpPassword,
+            },
+          },
+          defaults: {
+            from: `My Blog <no-reply@nestjs-blog.com>`,
+          },
+          template: {
+            dir: join(__dirname, 'templates'),
+            adapter: new EjsAdapter(),
+            options: {
+              strict: false,
+            },
+          },
+        };
+      },
+    }),
+  ],
   providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
