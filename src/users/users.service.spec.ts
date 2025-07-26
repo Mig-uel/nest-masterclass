767e835f7a536d00120ca90733b4aa20b87e444f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 08:47:52 -0400

    feat: enhance UsersService tests with mock provider
    - Added mock implementation for CreateUserProvider
    - Ensured create method is defined and functional in tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,20 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CreateGoogleUserProvider } from './providers/create-google-user.provider';
-import { UsersService } from './users.service';
-import { UsersCreateMany } from './providers/users-create-many';
-import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
-import { CreateUserProvider } from './providers/create-user.provider';
-import { FindOneUserByEmailProvider } from './providers/find-one-user-by-email';
-import { FindOneByGoogleIdProvider } from './providers/find-one-by-google-id.provider';
-import { DataSource } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
+import { DataSource } from 'typeorm';
 import profileConfig from './config/profile.config';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './entities/user.entity';
+import { CreateGoogleUserProvider } from './providers/create-google-user.provider';
+import { CreateUserProvider } from './providers/create-user.provider';
+import { FindOneByGoogleIdProvider } from './providers/find-one-by-google-id.provider';
+import { FindOneUserByEmailProvider } from './providers/find-one-user-by-email';
+import { UsersCreateMany } from './providers/users-create-many';
+import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
+    const mockCreateUserProvider: Partial<CreateUserProvider> = {
+      create(createUserDto: CreateUserDto) {
+        return Promise.resolve({
+          id: 'some-id',
+          firstName: createUserDto.firstName,
+          lastName: createUserDto.lastName,
+          email: createUserDto.email,
+          password: createUserDto.password,
+        });
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
@@ -36,7 +49,7 @@ describe('UsersService', () => {
         },
         {
           provide: CreateUserProvider,
-          useValue: {},
+          useValue: mockCreateUserProvider,
         },
         {
           provide: FindOneUserByEmailProvider,
@@ -62,5 +75,22 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should be defined', () => {
+      expect(service.create).toBeDefined();
+    });
+
+    it('should call create on CreateUserProvider', async () => {
+      const user = await service.create({
+        email: 'test@email.com',
+        firstName: 'John',
+        password: 'password',
+        lastName: 'Test',
+      });
+
+      expect(user.firstName).toEqual('John');
+    });
   });
 });
