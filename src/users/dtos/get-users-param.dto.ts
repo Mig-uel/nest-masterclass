4e930deef329bf32afbafa8d6541f0877dc6b980
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 11:26:24 -0400

    refactor: update GetUsersParamDto to use UUID for id
    - Changed id property type from optional number to required UUID string
    - Simplified imports by removing unused decorators and types

--- src/users/dtos/get-users-param.dto.ts
+++ src/users/dtos/get-users-param.dto.ts
@@ -1,12 +1,8 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsInt, IsOptional } from 'class-validator';
-
-import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsUUID } from 'class-validator';
 
 export class GetUsersParamDto {
   @ApiPropertyOptional()
-  @IsOptional()
-  @IsInt()
-  @Type(() => Number)
-  id?: number;
+  @IsUUID()
+  id: string;
 }
