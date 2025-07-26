41bb5010545a6104a6c00bad4e94525a58ffe95d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 16:29:08 -0400

    feat: implement create method in TagsController
    - Add create method to handle tag creation requests
    - Inject TagsService to facilitate interaction with the service layer
    - Utilize CreateTagDto for request validation

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { TagsService } from './providers/tags.service';
+import { CreateTagDto } from './dtos/create-tag.dto';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagsService: TagsService) {}
+
+  create(@Body() createTagDto: CreateTagDto) {
+    return this.tagsService.create(createTagDto);
+  }
+}
