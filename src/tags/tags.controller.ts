1625ea2846002bf17752cadc86f08a9bce11abb6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 16:31:47 -0400

    feat: add POST method to TagsController for tag creation
    - Introduced the @Post() decorator to enable tag creation
    - This allows clients to create new tags via HTTP POST requests

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller } from '@nestjs/common';
-import { TagsService } from './providers/tags.service';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateTagDto } from './dtos/create-tag.dto';
+import { TagsService } from './providers/tags.service';
 
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
+  @Post()
   create(@Body() createTagDto: CreateTagDto) {
     return this.tagsService.create(createTagDto);
   }
