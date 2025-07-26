7ea4e3a6f0885107b5d0e178a6f75dd42374c229
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 11:15:39 -0400

    refactor: streamline User schema properties
    - Consolidate property decorators for clarity
    - Maintain timestamps for user records

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,32 +1,23 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
 import { Document } from 'mongoose';
 
-@Schema()
+@Schema({
+  timestamps: true,
+})
 export class User extends Document {
-  // Note: id is not needed because _id is added by default by MongoDb
   @Prop({
     type: String,
     isRequired: true,
   })
   firstName: string;
 
-  @Prop({
-    type: String,
-    isRequired: false,
-  })
+  @Prop({ type: String, isRequired: false })
   lastName?: string;
 
-  @Prop({
-    type: String,
-    isRequired: true,
-  })
+  @Prop({ type: String, isRequired: true })
   email: string;
 
-  @Prop({
-    type: String,
-    isRequired: true,
-  })
+  @Prop({ type: String, isRequired: true })
   password: string;
 }
 
