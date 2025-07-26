778e9b181153772fc589f20a5a4bf599a6e05b90
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 11:42:54 -0400

    refactor: streamline user creation in UsersController
    - Simplified createUser method to directly call UsersService
    - Removed unnecessary console log for cleaner code

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import type { Model } from 'mongoose';
+import { User } from '../user.schema';
+import { CreateUserDto } from '../dtos/create-user.dto';
 /**
  * Class to connect to Users table and perform business operations
  */
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
   /**
    * The method to get all the users from the database
    */
@@ -29,5 +37,10 @@ export class UsersService {
       firstName: 'Alice',
       email: 'alice@doe.com',
     };
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    return await user.save();
   }
 }
