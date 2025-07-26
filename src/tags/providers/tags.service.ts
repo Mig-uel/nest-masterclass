65de91f5df52f09c88fa507d469a94ac6583d418
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-26 16:29:05 -0400

    feat: implement TagsService with create method
    - Add constructor to inject Tag model
    - Implement create method to save new tags

--- src/tags/providers/tags.service.ts
+++ src/tags/providers/tags.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import type { Model } from 'mongoose';
+import { Tag } from '../tag.schema';
+import { CreateTagDto } from '../dtos/create-tag.dto';
 
 @Injectable()
-export class TagsService {}
+export class TagsService {
+  constructor(
+    @InjectModel(Tag.name)
+    private readonly tagModel: Model<Tag>,
+  ) {}
+
+  async create(createTagDto: CreateTagDto) {
+    const tag = new this.tagModel(createTagDto);
+
+    return await tag.save();
+  }
+}
