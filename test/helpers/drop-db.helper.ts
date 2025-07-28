9ee3967bce350ef5b3c8cebb61e920811bcee86e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-28 11:57:17 -0400

    feat: add dropDB helper function for database management
    - Implements a utility to drop the database using TypeORM
    - Facilitates easier testing and database reset during development

---
+++ test/helpers/drop-db.helper.ts
@@ -0,0 +1,23 @@
+import { type ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+export async function dropDB(configService: ConfigService): Promise<void> {
+  // Create the connection datasource
+  const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: configService.get('database.host'),
+    username: configService.get('database.username'),
+    password: configService.get('database.password'),
+    database: configService.get('database.database'),
+    synchronize: configService.get('database.synchronize'),
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // Drop all tables
+  await AppDataSource.dropDatabase();
+
+  // Close the connection
+  await AppDataSource.destroy();
+}
