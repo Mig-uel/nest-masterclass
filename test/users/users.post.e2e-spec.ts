103b752c186af4457e88f0ad95544db6044bdd24
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-28 10:48:12 -0400

    test: add end-to-end tests for user creation endpoints
    - Implemented test cases for user-related POST endpoints
    - Ensures mandatory fields are validated and responses are correct

---
+++ test/users/users.post.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { type INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { App } from 'supertest/types';
+import { AppModule } from '../../src/app.module';
+
+describe('[Users] @Post Endpoints', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it.todo('/users - Endpoint is public');
+  it.todo('/users - firstName is mandatory');
+  it.todo('/users - email is mandatory');
+  it.todo('/users - password is mandatory');
+  it.todo('/users - Valid request successfully creates user');
+  it.todo('/users - password is not returned in response');
+  it.todo('/users - googleId is not returned in response');
+});
