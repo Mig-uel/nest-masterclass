1323b96761f4a2537e5d0271cf860cae23e1cba8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-07-28 12:18:16 -0400

    feat: implement user creation endpoint test
    - Added a test for the public access of the /users endpoint.
    - Ensured the test sends a request and logs the response.

--- test/users/users.post.e2e-spec.ts
+++ test/users/users.post.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { type INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import supertest from 'supertest';
 import { App } from 'supertest/types';
 import { bootstrapNestApp } from 'test/helpers/bootstrap-nest-app.helper';
 import { dropDB } from 'test/helpers/drop-db.helper';
@@ -8,12 +9,17 @@ describe('[Users] @Post Endpoints', () => {
   let app: INestApplication<App>;
   let config: ConfigService;
 
+  let httpServer: App;
+
   beforeEach(async () => {
     // Instantiating the application
     app = await bootstrapNestApp();
 
     // Extracting the config
     config = app.get<ConfigService>(ConfigService);
+
+    // Extract HTTP server
+    httpServer = app.getHttpServer();
   });
 
   afterEach(async () => {
@@ -21,7 +27,15 @@ describe('[Users] @Post Endpoints', () => {
     await app.close();
   });
 
-  it.todo('/users - Endpoint is public');
+  it('/users - Endpoint is public', () => {
+    return supertest(httpServer)
+      .post('/users')
+      .send({})
+      .then((data) => {
+        console.log(data);
+      });
+  });
+
   it.todo('/users - firstName is mandatory');
   it.todo('/users - email is mandatory');
   it.todo('/users - password is mandatory');
